f6036b8c0ee3451437967d30ae5fbcfcbadb75f0
Cyrus Yip <cyruscmyip1@gmail.com>
2021-12-19 14:09:10 -0800

    change up the hover

--- src/components/Song.vue
+++ src/components/Song.vue
@@ -7,7 +7,7 @@ defineProps({
 </script>
 
 <template>
-    <div class="mb-3 p-3 sm:p-5 rounded-lg w-full flex flex-row bg-white bg-opacity-20 bg-clip-padding backdrop-blur-lg hover:bg-opacity-50 duration-500">
+    <div class="mb-3 p-3 sm:p-5 rounded-lg w-full flex flex-row bg-white bg-opacity-20 bg-clip-padding backdrop-blur-lg hover:bg-opacity-30 duration-500">
         <a
             :href="'https://www.youtube.com/watch?v=' + song.id + '&list=PLRct1-5In-8Ewg5Kq-0JP8wh3ZweOXH9A'"
             class="w-[120px] h-[67px] sm:w-[180px] sm:h-[100px] mr-5 flex-none my-2 sm:my-0 relative"
@@ -22,11 +22,11 @@ defineProps({
         <div class="flex flex-col justify-between items-start">
             <a
                 :href="'https://www.youtube.com/watch?v=' + song.id + '&list=PLRct1-5In-8Ewg5Kq-0JP8wh3ZweOXH9A'"
-                class="text-white text-xs sm:text-base hover:text-blue-200"
+                class="text-white text-xs sm:text-base hover:text-blue-200 duration-500"
             >{{ song.title }}</a>
             <a
                 :href="'https://www.youtube.com/channel/' + song.channelId"
-                class="text-white text-xs sm:text-base hover:text-blue-200"
+                class="text-white text-xs sm:text-base hover:text-blue-200 duration-500"
             >{{ song.channel }}</a>
         </div>
     </div>
